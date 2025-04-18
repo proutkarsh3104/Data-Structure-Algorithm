3f55caafa554127a9b5df77066147832c6017c2e
Utkarsh Tiwari <utkarsh3104.imp@gmail.com>
2025-04-19 00:22:13 +0530

    finished stack

---
+++ 31-Stack-Pop.py
@@ -0,0 +1,11 @@
+def pop(self):
+    if self.height == 0:
+        return None
+    temp = self.top
+    self.top = self.top.next
+    temp.next = None
+    self.height -= 1
+    return temp
+    
+    
+    
