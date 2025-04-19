3f565c7fba2b2351a4c50bd783bbeed06e4c8736
Utkarsh Tiwari <utkarsh3104.imp@gmail.com>
2025-04-19 11:47:57 +0530

    finished queue

---
+++ 33-Queue_Enqueue.py
@@ -0,0 +1,9 @@
+def enqueue(self, value):
+    new_node = Node(value)
+    if self.first is None:
+        self.first = new_node
+        self.last = new_node
+    else:
+        self.last.next = new_node
+        self.last = new_node
+    self.length += 1
